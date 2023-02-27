a3b317572e66cfedf644c2bea514d5be588e5777
Karanxidhu <sidhu.karanjot22@gmail.com>
2023-02-27 21:46:17 +0530

    Create run.ps1

---
+++ node js/run.ps1
@@ -0,0 +1,17 @@
+# Define the repository URL and file path
+$repoUrl = "https://github.com/Karanxidhu/screenshots.git"
+$filePath = "node js/screenshot.js"
+
+# Clone the repository to a temporary directory
+$tempDir = New-Item -ItemType Directory -Path $env:TEMP\GitDownload -Force
+git clone $repoUrl $tempDir.FullName
+
+# Navigate to the repository directory and checkout the default branch
+cd $tempDir.FullName
+# git checkout master
+
+# Copy the file to the current directory
+# Copy-Item $filePath .
+
+# Clean up the temporary directory
+# Remove-Item $tempDir.FullName -Recurse -Force
